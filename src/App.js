c526c3361d821c509cfe5885b333bc68a82735f1
ihk-01 <ingridkilmer@gmail.com>
2023-02-24 16:43:11 -0500

    fashion blog in React

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import Article from './Components/Article.js';
+import Footer from './Components/Footer.js';
+import Header from './Components/Header.js';
+import Nav from './Components/Nav.js';
+import './style.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header/>
+      <Nav/>
+      <Article/>
+      <Footer/>
+
+      
+
+
     </div>
+    
   );
 }
 
---
+++ src/Components/Article.js
@@ -0,0 +1,34 @@
+import blogimage1 from './img/blogimage1.jpg';
+import blogimage2 from './img/blogimage2.jpg';
+
+function Article (){
+    return (
+
+<main>
+            <section>
+                <article>
+                    <time datetime='2020-11-12'>12 Nov 2020</time>
+                    <h3>On the Street in Brooklyn</h3>
+                    <img src={blogimage1} alt='Woman on street next to buildings' />
+                    <p className='has-dropcap'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde optio cumque quibusdam nisi ad consequatur deleniti officia necessitatibus sunt quasi dolore, ab similique voluptates, quos veritatis! Amet omnis culpa incidunt dolorum ea tempore reprehenderit placeat quo, libero molestias pariatur, blanditiis sed consequuntur itaque architecto accusamus. Tempore fugit eius assumenda dolorem et illo voluptate veniam temporibus error expedita sequi quasi necessitatibus consequatur ratione iusto, corrupti repellat perspiciatis dolore in id at explicabo consequuntur, enim ducimus! Expedita magnam possimus accusantium, molestiae eaque ipsum at neque. Quidem quaerat sequi, inventore iure neque amet consequatur tempore! Fugit perferendis sequi cumque dolore dolorum rem repellat. Laborum accusamus quaerat eos, nemo eveniet molestiae dicta nesciunt obcaecati earum voluptate illum aliquam magni ipsum repellat cupiditate ipsam dolore repudiandae, recusandae voluptatum quis vel temporibus error sapiente quam. Facilis, natus, impedit architecto explicabo, amet reiciendis quas veniam magni necessitatibus delectus dolorum odit? Harum, maxime quod. Nobis, accusantium iusto! Atque iste delectus a ad, accusantium consequatur earum. Esse quo recusandae itaque eum cum quia deleniti voluptatem? Ex aut obcaecati ratione necessitatibus provident itaque nulla labore ut minima, quam tenetur explicabo perspiciatis dicta, adipisci unde similique corporis omnis assumenda, quasi consequatur. Perspiciatis sunt ea quibusdam? Sed, quo sint. Expedita, nisi quos?</p>
+                </article>
+                <div className='continues'>
+                    <p>Continues...</p>
+                </div>
+                <hr />
+            </section>
+            <section>
+                <article>
+                    <time datetime='2020-11-11'>11 Nov 2020</time>
+                    <h3>Vintage in Vogue</h3>
+                    <img src={blogimage2} alt='Headshot of two women and a man' />
+                    <p className='has-dropcap'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, eaque! Iste maxime excepturi illum dolor distinctio ex corporis esse blanditiis tempora quis aliquam, facilis unde commodi quia error id odit nemo facere ipsam harum hic! Temporibus voluptates recusandae minima fuga molestias ducimus eius praesentium omnis officiis voluptas autem natus magni laudantium rerum numquam, a suscipit maxime tenetur sunt et doloribus blanditiis veniam vitae? Neque voluptatum laudantium, rem autem praesentium ab perspiciatis vitae officiis sequi porro itaque commodi nostrum expedita obcaecati ipsam odit! Expedita corporis dolor fuga, voluptatem labore debitis veritatis quae alias omnis, aspernatur excepturi at tempora fugit ullam adipisci dolores tempore molestiae commodi minus sapiente facere doloremque beatae ipsam itaque. Est sapiente saepe vitae sed mollitia tempore, unde quasi voluptatibus sunt a impedit ipsa dolores odio autem quaerat nam qui ipsum tenetur iusto quam repudiandae eius voluptatum suscipit. Magnam ipsa odio minus magni quo dolores sed atque sunt illo porro voluptates sequi dicta nihil ex non dolor accusantium natus quod nemo laboriosam, ratione, quas tempora. Unde obcaecati omnis dicta deleniti dolorum quae corrupti sed velit ratione minima, qui maiores nam tenetur id mollitia eum cumque error similique numquam? Maxime beatae repellendus culpa corporis maiores laboriosam officia ut ipsa dicta.</p>
+                </article>
+                <div className='continues'>
+                    <p>Continues...</p>
+                </div>
+                <hr />
+            </section>
+        </main>
+    )}
+        export default Article;
